cb7d9f2ee84f9b1d47ee29ced917d680c364ccfc
Mateo Ochandorena <mateochando@gmail.com>
2023-08-22 09:54:44 +0200

    Update Environment.kt

--- src/main/kotlin/com/example/plugins/Environment.kt
+++ src/main/kotlin/com/example/plugins/Environment.kt
@@ -3,6 +3,10 @@ package com.example.plugins
 import io.github.cdimascio.dotenv.dotenv
 
 class Environment {
+    val env = dotenv {
+        ignoreIfMalformed = true
+        ignoreIfMissing = true
+    }
     fun getVariable(name: String, fallbackValue: String = ""): String =
-        (System.getenv(name) ?: dotenv()[name] ?: fallbackValue)
+        (System.getenv(name) ?: env[name] ?: fallbackValue)
 }
